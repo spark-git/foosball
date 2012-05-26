08aec58570b9444911f0d0cf5acd036eefb05398
Sébastien Le Callonnec <sebastien@weblogism.com>
2012-05-26 09:14:28 +0100

    Connecting to mongodb

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,37 +1,58 @@
+// Module dependencies
+var express = require('express');
+var routes = require('./routes');
 
-/**
- * Module dependencies.
- */
 
-var express = require('express')
-  , routes = require('./routes');
+var PlayerProvider = require('./playerprovider').PlayerProvider;
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+app.configure(
+    function(){
+        app.set('views', __dirname + '/views');
+        app.set('view engine', 'jade');
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+        app.use(app.router);
+        app.use(express.static(__dirname + '/public'));
+    }
+);
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+app.configure(
+    'development',
+    function() {
+        app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    }
+);
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
+app.configure(
+    'production',
+    function() {
+        app.use(express.errorHandler());
+    }
+);
+
+var playerProvider = new PlayerProvider('localhost', 27017);
 
 // Routes
 
-app.get('/', routes.index);
+app.get(
+    '/', 
+    function(req, res) {
+        playerProvider.findAll(
+            function(error, docs) {
+                res.render('index.jade', { locals: { title: 'Foosball', players: docs}});
+            }
+        );
+    }
+);
 
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
+app.listen(
+    3000, 
+    function() {
+        console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+    }
+);
---
+++ nodejs/playerprovider.js
@@ -0,0 +1,83 @@
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
+var BSON = require('mongodb').BSON;
+var ObjectID = require('mongodb').ObjectID;
+
+PlayerProvider = function(host, port) {
+    this.db = new Db('node-mongo-foosball', new Server(host, port, {auto_reconnect: true}, {}));
+    this.db.open(function() {});
+};
+
+// returns a collection of players
+PlayerProvider.prototype.getCollection = function(callback) {
+    this.db.collection(
+        'players',
+        function(error, player_collection) {
+            if (error) callback(error);
+            else
+                callback(null, player_collection);
+        }
+    );  
+};
+
+PlayerProvider.prototype.findAll = function(callback) {
+    this.getCollection(
+        function(error, player_collection) {
+            if (error) callback(error);
+            else {
+                player_collection.find().toArray(
+                    function(error, results) {
+                        if (error) callback(error);
+                        else callback(null, results);
+                    }
+                );
+            }
+        }
+    );
+};
+
+PlayerProvider.prototype.findById = function(id, callback) {
+    this.getCollection(
+        function(error, player_collection) {
+            if (error) callback(error);
+            else {
+                player_collection.findOne(
+                    {_id: player_collection.db.bson_serializer.ObjectID.createFromHexString(id)},
+                    function(error, result) {
+                        if (error) callback(error);
+                        else callback(null, result);
+                    }
+                );
+            }
+        }
+    );   
+
+};
+
+PlayerProvider.prototype.save = function(players, callback) {
+    this.getCollection(
+        function(error, player_collection) {
+            if (error) callback(error);
+            else {
+                // Set player into array if it isn't one.
+                if (typeof(players.length) == "undefined")
+                    players = [players];
+                
+                for (var i = 0; i < players.length; i++) {
+                    player = players[i];
+                    player.created_at = new Date();
+                }
+
+                player_collection.insert(
+                    players, 
+                    function() {
+                        callback(null, players);
+                    }
+                );
+            }
+        }
+    );    
+};
+
+exports.PlayerProvider = PlayerProvider;
